9b065c1843e23f116fe8df1f1b731afa1e52b980
Carolina Veloso <annacarolinacampos@hotmail.com>
2024-03-27 12:23:25 -0300

    ajuste método ask champion

--- src/main/java/carolina/sdw24/application/AskChampionsUseCase.java
+++ src/main/java/carolina/sdw24/application/AskChampionsUseCase.java
@@ -10,7 +10,8 @@ public record AskChampionsUseCase(ChampionsRepository repository) {
         Champion champion = repository.findById(championId)
                 .orElseThrow(() -> new ChampionNotFoundException(championId));
 
+        String context = champion.generateContextByQuestion(champion, question);
         // TODO : Evoluir a lógica para considerar a integração com IA
-        return "";
+        return context;
     }
 }
