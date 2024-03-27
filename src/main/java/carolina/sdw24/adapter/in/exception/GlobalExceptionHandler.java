bc731bba4619bc7fa884edd7acd269e374821380
Carolina Veloso <annacarolinacampos@hotmail.com>
2024-03-27 17:11:41 -0300

    Criação GlobalExceptionHandler

--- src/main/java/carolina/sdw24/adapter/in/exception/GlobalExceptionHandler.java
+++ src/main/java/carolina/sdw24/adapter/in/exception/GlobalExceptionHandler.java
@@ -1,14 +1,11 @@
-package carolina.sdw24.adapter.in;
+package carolina.sdw24.adapter.in.exception;
 
 import carolina.sdw24.domain.exception.ChampionNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
