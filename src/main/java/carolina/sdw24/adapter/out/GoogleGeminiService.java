b1c200f824f0a47e8c97d0db6c75482d05829758
Carolina Veloso <annacarolinacampos@hotmail.com>
2024-03-28 08:50:50 -0300

    Integração com Api Google Gemini com tratamento de erros

---
+++ src/main/java/carolina/sdw24/adapter/out/GoogleGeminiService.java
@@ -0,0 +1,57 @@
+package carolina.sdw24.adapter.out;
+
+import carolina.sdw24.domain.ports.GenerativeAiService;
+import feign.FeignException;
+import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+
+@FeignClient(name = "geminiApi", url = "${gemini.base-url}", configuration = GoogleGeminiService.Config.class)
+public interface GoogleGeminiService extends GenerativeAiService {
+
+    @PostMapping("/v1beta/models/gemini-pro:generateContent")
+    GoogleGeminiResp textOnlyInput(GoogleGeminiReq request);
+
+    @Override
+    default String generateContent(String objective, String context) {
+        String prompt = """
+                %s
+                %s
+                """.formatted(objective, context);
+
+        GoogleGeminiReq req = new GoogleGeminiReq(
+                List.of(new Content(List.of(new Part(prompt))))
+        );
+        try {
+            GoogleGeminiResp resp = textOnlyInput(req);
+            return resp.candidates().getFirst().content().parts().getFirst().text();
+        } catch (FeignException httpErrors) {
+            return "Deu ruim! Erro de comunicação com a API do Google Gemini";
+        } catch (Exception unexpectedError) {
+            return "Deu mais ruim ainda! O retorno da API do Google Gemini não contém os dados esperados";
+        }
+    }
+    //Request
+    record GoogleGeminiReq(List<Content> contents) {}
+    record Content(List<Part> parts) {}
+    record Part(String text) {}
+    //Response
+    record GoogleGeminiResp(List<Candidate> candidates) {}
+    record Candidate(Content content){}
+
+    class Config{
+        @Bean
+        public RequestInterceptor apiKeyRequestInterceptor(@Value("${openai.api-key}") String apiKey){
+            return requestTemplate -> requestTemplate.query("key", apiKey);
+        }
+    }
+
+
+
+}
